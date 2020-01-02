8d5104866509a2a0b5b615d56b4ca7bb1a757c60
Jeffreypir <jefferson1995mat@gmail.com>
2020-01-02 19:32:27 -0300

    template

---
+++ backup/template/template.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define EXIT_SUCESS 0
+
+/*==================== PROGRAM ==============================
+ * Program: <+name+>
+ * Description: <+description+>
+ * Date of Create: <+date+>
+ * Update in: <+update+>
+ * Author:Jefferson Bezerra dos Santos
+ *===========================================================
+ */
+
+
+/*=================== FUNCTION MAIN () ======================
+ * Function: main()
+ * Description: <+description+>
+ * ==========================================================
+ */
+int main(<+argument+>){
+		return EXIT_SUCESS;
+}/*----------End of function-----------------*/
